1bcb2ead60dd51d06570439fca1a4fbb739e6d0d
GK <gajelli.kiransai@gmail.com>
2024-10-01 19:11:52 +0530

    user auth created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,56 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+import { error } from 'console';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-
-@Injectable ()
+@Injectable()
 export class AuthService {
-    
-    constructor(private prisma: PrismaService){
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+        throw error;
+      }
+    }
+  }
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
 
-    }
-    signin() {
-        return {msg: 'I am signed in'}
-    }
-    signup() {
-        return {msg: 'I am signin up'}
-    }
+    // if userdoesnot exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // if password incorrect throw exception
+    if(!pwMatches) throw new ForbiddenException('Credentials incorrect')
+    // send the user
+    return user
+  }
 }
