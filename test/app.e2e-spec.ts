3fb73eed10bbb65b9ebe1428168b6ffd35bdc056
GK <gajelli.kiransai@gmail.com>
2024-10-03 11:37:33 +0530

    scripts and docker-compose.yml file created for dev and test db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,30 @@
-import {Test} from '@nestjs/testing'
-import { AppModule } from 'src/app.module'
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('app e2e', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule]
+      imports: [AppModule],
     }).compile();
-  })
-  it.todo('should pass')
-})
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+  });
+  afterAll(() => {
+    app.close();
+  });
+  describe('Auth', () => {})
+  describe('User', () => {})
+  describe('Bookmarks', ()=>{})
+});
